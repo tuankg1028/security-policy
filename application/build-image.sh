7fab7ac5f5760098a02cac726436f395eb32ff0a
Xander <xander@vinova.com.sg>
2021-08-01 17:42:14 +0700

    fix

--- application/build-image.sh
+++ application/build-image.sh
@@ -6,4 +6,4 @@ set -e
 #docker build -t edgify:0.1.0 ./base
 # cd code && npm run build
 # cd ..
-docker build --no-cache -t security-policy:0.0.1 .
+docker build --no-cache -t security-policy-scone:0.0.1 .
